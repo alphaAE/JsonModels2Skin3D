83b4465a7fe3de73c5ec4425b0fc5a12e6644e98
alpha AE <1226123914@qq.com>
2019-09-20 22:09:04 +0800

    v0.0.1

--- JsonModels2Skin3D/Form1.Designer.cs
+++ JsonModels2Skin3D/Form1.Designer.cs
@@ -25,14 +25,18 @@
         private void InitializeComponent() {
             this.selectFileBtn = new System.Windows.Forms.Button();
             this.Versionlabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Descriptionlabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // selectFileBtn
             // 
             this.selectFileBtn.Font = new System.Drawing.Font("宋体", 15.85714F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.selectFileBtn.Location = new System.Drawing.Point(254, 12);
+            this.selectFileBtn.Location = new System.Drawing.Point(13, 7);
+            this.selectFileBtn.Margin = new System.Windows.Forms.Padding(2);
             this.selectFileBtn.Name = "selectFileBtn";
-            this.selectFileBtn.Size = new System.Drawing.Size(291, 96);
+            this.selectFileBtn.Size = new System.Drawing.Size(159, 55);
             this.selectFileBtn.TabIndex = 0;
             this.selectFileBtn.Text = "选择文件";
             this.selectFileBtn.UseVisualStyleBackColor = true;
@@ -41,22 +45,48 @@
             // Versionlabel
             // 
             this.Versionlabel.AutoSize = true;
-            this.Versionlabel.Location = new System.Drawing.Point(712, 338);
+            this.Versionlabel.Location = new System.Drawing.Point(455, 189);
+            this.Versionlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Versionlabel.Name = "Versionlabel";
-            this.Versionlabel.Size = new System.Drawing.Size(76, 21);
+            this.Versionlabel.Size = new System.Drawing.Size(89, 12);
             this.Versionlabel.TabIndex = 1;
-            this.Versionlabel.Text = "v0.0.0";
+            this.Versionlabel.Text = "alphaae v0.0.0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Controls.Add(this.Descriptionlabel);
+            this.groupBox1.Location = new System.Drawing.Point(13, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(531, 119);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "说明";
+            // 
+            // Descriptionlabel
+            // 
+            this.Descriptionlabel.AutoSize = true;
+            this.Descriptionlabel.Location = new System.Drawing.Point(7, 21);
+            this.Descriptionlabel.Name = "Descriptionlabel";
+            this.Descriptionlabel.Size = new System.Drawing.Size(101, 12);
+            this.Descriptionlabel.TabIndex = 0;
+            this.Descriptionlabel.Text = "Descriptionlabel";
             // 
             // JsonModels2Skin3D
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 368);
+            this.ClientSize = new System.Drawing.Size(555, 210);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.Versionlabel);
             this.Controls.Add(this.selectFileBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "JsonModels2Skin3D";
             this.Text = "JsonModels2Skin3D";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,6 +96,8 @@
 
         private System.Windows.Forms.Button selectFileBtn;
         private System.Windows.Forms.Label Versionlabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label Descriptionlabel;
     }
 }
 
--- JsonModels2Skin3D/Form1.cs
+++ JsonModels2Skin3D/Form1.cs
@@ -14,47 +14,60 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using static JsonModels2Skin3D.model.McGeometryJson;
 
-namespace JsonModels2Skin3D {
-    public partial class JsonModels2Skin3D : Form {
-
+namespace JsonModels2Skin3D
+{
+    public partial class JsonModels2Skin3D : Form
+    {
+        public string authorText = "alphaAE";
         public string versionText = "v0.0.1";
+        public string descriptionText = "选择基岩版Json实体模型，将在同一文件夹内生成Xml格式的Skin3D模型文件\n\n" +
+            "注意事项：\n" +
+            "\t目前仅支持低于1.12老版本Json实体模型文件\n" +
+            "\t由于Skin3D模型定义旋转原点失效，建模过程中若用到旋转请将原点放在[0,0,0]\n";
 
-        public JsonModels2Skin3D() {
+        public JsonModels2Skin3D()
+        {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e) {
-            this.Versionlabel.Text = versionText;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Versionlabel.Text = authorText + " " + versionText;
+            this.Descriptionlabel.Text = descriptionText;
         }
 
-        private void SelectFileBtn_Click(object sender, EventArgs e) {
+        private void SelectFileBtn_Click(object sender, EventArgs e)
+        {
             OpenFileDialog fileDialog = new OpenFileDialog();
             fileDialog.Multiselect = true;
             fileDialog.Title = "请选择模型Json文件";
             fileDialog.Filter = "所有文件(*.json)|*.json";
-            if (fileDialog.ShowDialog() == DialogResult.OK) {
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
                 string file = fileDialog.FileName;
                 ToSkin3DFile(file);
             }
         }
 
-        private Boolean ToSkin3DFile(string file) {
+        private Boolean ToSkin3DFile(string file)
+        {
             string content = File.ReadAllText(file);
             JObject jobj = JObject.Parse(content);
 
             //遍历最外层识别实体个数
             List<string> modelList = new List<string>();
-            foreach (var item in jobj) {
+            foreach (var item in jobj)
+            {
                 if (item.Key == "format_version") continue;
                 modelList.Add(item.Key);
             }
-            MessageBox.Show(string.Join("\n", modelList.ToArray()), "识别到实体", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
             //挨个取出序列化
-            foreach (string key in modelList) {
-                try {
+            foreach (string key in modelList)
+            {
+                try
+                {
                     McGeometryJson personlist = JsonConvert.DeserializeObject<McGeometryJson>(jobj.GetValue(key).ToString());
-                    Console.WriteLine(personlist.bones.Length);
 
 
                     //Skin3DXml init
@@ -72,23 +85,55 @@ namespace JsonModels2Skin3D {
                     xmlObject.Models.Model.TextureSize = "" + personlist.texturewidth + "," + personlist.textureheight;
 
                     //
-                    foreach (Bone bone in personlist.bones) {
-                        int cubeNum = 0;
-                        try {
-                            foreach (Cube cube in bone.cubes) {
+                    foreach (Bone bone in personlist.bones)
+                    {
+                        try
+                        {
+
+                            foreach (Cube cube in bone.cubes)
+                            {
                                 //修复坐标Y反转
                                 cube.origin[1] = ((cube.origin[1] + (cube.size[1] / 2)) * -1) - (cube.size[1] / 2);
+                                //防止旋转为空
+                                float[] rotation = bone.rotation == null ? new float[] { 0, 0, 0 } : bone.rotation;
+
 
                                 Skin3dModelXml.TechneModelsModelGeometryFolderShape tempShape = new Skin3dModelXml.TechneModelsModelGeometryFolderShape();
                                 tempShape.Type = "d9e621f7-957f-4b77-b1ae-20dcd0da7751";
-                                tempShape.Name = bone.name + cubeNum++;
+                                tempShape.Name = bone.name + "-" + Guid.NewGuid().ToString("N");    //去重
                                 tempShape.IsDecorative = "False";
                                 tempShape.IsFixed = "False";
                                 tempShape.IsMirrored = "" + (bone.mirror || cube.mirror);
                                 tempShape.IsSolid = "False";
-                                tempShape.Offset = "0,0,0"; //暂时使用同一原点
-                                tempShape.Position = string.Join(",", cube.origin);
-                                tempShape.Rotation = bone.rotation == null ? "0,0,0" : string.Join(",", bone.rotation);
+                                tempShape.Offset = string.Join(",", cube.origin);
+                                tempShape.Position = String.Format("0,0,0");
+
+                                // 继承父级旋转
+                                if (bone.parent != null)
+                                {
+                                    foreach (Bone findParentBone in personlist.bones)
+                                    {
+                                        if (findParentBone.name == bone.parent)
+                                        {
+                                            try
+                                            {
+
+                                                float[] parentRotation = findParentBone.rotation == null ? new float[] { 0, 0, 0 } : findParentBone.rotation;
+                                                Console.WriteLine(bone.parent + ":" + bone.name + "=>" + rotation.Length + " =>" + parentRotation.Length);
+                                                rotation[0] += parentRotation[0];
+                                                rotation[1] += parentRotation[1];
+                                                rotation[2] += parentRotation[2];
+                                            }
+                                            catch (Exception e)
+                                            {
+                                                Console.WriteLine(e.ToString());
+                                            }
+
+                                        }
+                                    }
+                                }
+
+                                tempShape.Rotation = string.Join(",", rotation);
                                 tempShape.Size = string.Join(",", cube.size);
                                 tempShape.TextureOffset = string.Join(",", cube.uv);
                                 tempShape.Scale = (decimal)cube.inflate;
@@ -96,9 +141,13 @@ namespace JsonModels2Skin3D {
                                 tempShape.Hidden = "False";
                                 tempShape.IsArmor = "False";
 
+
+
                                 shapelist.Add(tempShape);
                             }
-                        } catch (Exception e) {
+                        }
+                        catch (Exception e)
+                        {
                             Console.WriteLine(e.ToString());
                             continue;
                         }
@@ -112,14 +161,18 @@ namespace JsonModels2Skin3D {
                     Utils.SaveString2File(new FileInfo(file).DirectoryName + "/" + key + ".xml", xmlString);
 
                     //仅测试一个即退出
-                    break;
-                } catch (Exception e) {
-                    Console.WriteLine(e.ToString());
+                    //break;
                 }
-
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    MessageBox.Show(e.ToString(), "出现错误", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return false;
+                }
             }
-
+            MessageBox.Show("成功处理下列实体：\n\n" + string.Join("\n", modelList.ToArray()), "处理完成", MessageBoxButtons.OK, MessageBoxIcon.Information);
             return true;
         }
+
     }
 }
